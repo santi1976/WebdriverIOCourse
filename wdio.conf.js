74ba4c61573bf0f8daeeff237dc318a82dd525ea
Santiago R <santiso@gmail.com>
2020-11-15 03:19:01 -0300

    add jenkins.war to gitignore

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
-    specs: ['./tests/e2e-login.js'],
+    specs: ['./tests/**.js'],
     // Patterns to exclude.
     exclude: [],
 
